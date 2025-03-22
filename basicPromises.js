9e50aca2f24f6aa170e6c7f8b149333ec98b1d70
Anup Dhondi <anupdhondi5@gmail.com>
2025-03-23 00:48:57 +0530

    basics

--- basicPromises.js
+++ basicPromises.js
@@ -20,3 +20,9 @@ function promiseWithVariable(text) {
 promiseWithVariable("BOOM BOOM").then((msg) => {
   console.log(msg);
 });
+
+new Promise((resolve, reject) => {
+  resolve("I want to resolve this...");
+})
+  .then((msg) => console.log(msg))
+  .finally(() => console.log("I will finally run you"));
